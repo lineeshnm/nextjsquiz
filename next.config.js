a15819879dd08f9aecdbc48a38cd808b1e6743eb
Lineesh NM <lineesh2009@gmail.com>
2022-02-14 08:16:55 +0530

    VERCEL URL

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    URL: process.env.NODE_ENV === 'production' ? 'https://productionurl' : 'http://localhost:3000',
-    MONGODB_URI : "mongodb+srv://taskapp:taskapptaskapp@cluster0.ejphg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    URL: process.env.NODE_ENV === 'production' ? process.env.VERCEL_URL : 'http://localhost:3000',
     MONGODB_DB: "nextjsquiz",
     APP_NAME: "Lineesh QuizApp"
   }
